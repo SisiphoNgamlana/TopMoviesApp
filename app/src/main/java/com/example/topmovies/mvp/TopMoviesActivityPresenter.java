7c6eafc6ffe1d4e688b4dff9b9a246c475cc5cd4
SisiphoNgamlana <watziznaim@gmail.com>
2022-05-13 05:39:47 +0200

    Implement presenter

---
+++ app/src/main/java/com/example/topmovies/mvp/TopMoviesActivityPresenter.java
@@ -0,0 +1,18 @@
+package com.example.topmovies.mvp;
+
+public class TopMoviesActivityPresenter implements TopMoviesActivityMVP.presenter {
+    @Override
+    public void loadData() {
+
+    }
+
+    @Override
+    public void Unsubscribe() {
+
+    }
+
+    @Override
+    public void setView(TopMoviesActivityMVP.View view) {
+
+    }
+}
