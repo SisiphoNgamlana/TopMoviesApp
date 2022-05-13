bd725f8b09810bfc7cb620e8fc464a712d5aa501
SisiphoNgamlana <watziznaim@gmail.com>
2022-05-13 13:35:22 +0200

    Add TopMoviesActivityModule

---
+++ app/src/main/java/com/example/topmovies/mvp/TopMoviesActivityModule.java
@@ -0,0 +1,27 @@
+package com.example.topmovies.mvp;
+
+import com.example.topmovies.api.MovieAPIService;
+import com.example.topmovies.api.MoviesInfoService;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class TopMoviesActivityModule {
+
+    @Provides
+    public TopMoviesActivityMVP.Presenter providesPresenter(TopMoviesActivityMVP.Model model) {
+        return new TopMoviesActivityPresenter(model);
+    }
+
+    @Provides
+    public TopMoviesActivityMVP.Model providesModel(Repository repository) {
+        return new TopMoviesActivityModel(repository);
+
+    }
+
+    @Provides
+    public Repository providesRepository(MovieAPIService movieAPIService, MoviesInfoService moviesInfoService) {
+        return new TopMoviesRepository(movieAPIService, moviesInfoService);
+    }
+}
