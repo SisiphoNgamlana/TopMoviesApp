88752e91740eed687f71b1a13e60a01ff7517fa2
SisiphoNgamlana <watziznaim@gmail.com>
2022-05-16 09:01:07 +0200

    Implement presenter

--- app/src/main/java/com/example/topmovies/mvp/TopMoviesActivityPresenter.java
+++ app/src/main/java/com/example/topmovies/mvp/TopMoviesActivityPresenter.java
@@ -1,8 +1,15 @@
 package com.example.topmovies.mvp;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
 public class TopMoviesActivityPresenter implements TopMoviesActivityMVP.Presenter {
 
     private final TopMoviesActivityMVP.Model model;
+    private TopMoviesActivityMVP.View topMoviesView;
+    private Disposable subscription;
 
     public TopMoviesActivityPresenter(TopMoviesActivityMVP.Model model) {
         this.model = model;
@@ -10,16 +17,41 @@ public class TopMoviesActivityPresenter implements TopMoviesActivityMVP.Presente
 
     @Override
     public void loadData() {
+        subscription = model.result()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableObserver<TopMoviesViewModel>() {
+                    @Override
+                    public void onNext(TopMoviesViewModel topMoviesViewModel) {
+                        if (topMoviesView != null) {
+                            topMoviesView.updateData(topMoviesViewModel);
+                        }
+                    }
 
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                        if (topMoviesView != null) {
+                            topMoviesView.showSnackBar("Error getting movies");
+                        }
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 
     @Override
     public void Unsubscribe() {
-
+        if (subscription != null) {
+            subscription.dispose();
+        }
     }
 
     @Override
     public void setView(TopMoviesActivityMVP.View view) {
-
+        this.topMoviesView = view;
     }
 }
